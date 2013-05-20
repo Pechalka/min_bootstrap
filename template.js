67fb20a8e2f5d0776e19f3005208040f5f309381
Andrew <luzkovskyandrey@gmail.com>
2013-05-21 02:21:11 +0300

    init commit

---
+++ template.js
@@ -0,0 +1,43 @@
+
+'use strict';
+
+// Basic template description.
+exports.description = 'generate simple project with express jquery knockout twitter.bootstrap';
+
+// Template-specific notes to be displayed before question prompts.
+exports.notes = 'generate simple project with express jquery knockout twitter.bootstrap';
+
+// Template-specific notes to be displayed after question prompts.
+exports.after = 'now: npm install, npm start';
+
+// Any existing file or directory matching this wildcard will cause a warning.
+exports.warnOn = '*';
+
+// The actual init template.
+exports.template = function(grunt, init, done) {
+
+  init.process({type: 'node'}, [
+     // init.prompt('name'),
+     // init.prompt('version'),
+  ], function(err, props) {
+
+    props.dependencies = {
+      'express': '3.2.3'
+    };
+
+    props.template_test = "demo";
+    props.version =  "0.1.0";
+
+    var files = init.filesToCopy(props);
+
+    // Actually copy (and process) files.    
+    init.copyAndProcess(files, props, { noProcess: ['*.ai', '*.jpg'] });
+ 
+    // Generate package.json file.
+    init.writePackageJSON('package.json', props);
+
+    // All done!
+    done();
+  });
+
+};
